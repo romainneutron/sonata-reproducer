f83400bff4dd4d97e93a745a7d3021564fc93e17
Romain Neutron <imprec@gmail.com>
2016-05-31 14:57:05 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+sonatatest
+==========
+
+A Symfony project created on May 31, 2016, 12:42 pm.
---
+++ src/AppBundle/Admin/Block/GraphsBlockService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Admin\Block;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\CoreBundle\Validator\ErrorElement;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Sonata\BlockBundle\Block\BaseBlockService;
+
+class GraphsBlockService extends BaseBlockService
+{
+    public function getName()
+    {
+        return 'App Graphs';
+    }
+
+    public function setDefaultSettings(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'title' => 'App Graphs',
+            'template' => '@App/Admin/Block/app-graphs.html.twig',
+            'metric_endpoint' => 'https://graphite.monitoring',
+            'metric_prefix' => null,
+        ]);
+    }
+
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
+    {
+        $formMapper->add('settings', 'sonata_type_immutable_array', [
+            'keys' => [
+                ['title', 'text', ['required' => false]],
+            ],
+        ]);
+    }
+
+    public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
+    {
+        $errorElement
+            ->with('settings[title]')
+            ->assertNotNull(array())
+            ->assertNotBlank()
+            ->assertMaxLength(array('limit' => 50))
+            ->end();
+    }
+
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        return $this->renderResponse($blockContext->getTemplate(), [
+            'block' => $blockContext->getBlock(),
+            'settings' => $blockContext->getSettings(),
+        ], $response);
+    }
+}
